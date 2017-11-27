02e0c513552a8161bea0cda3c7250a36fa856b63
Jim Nasby <Jim.Nasby@GE.com>
2017-11-27 16:48:34 -0600

    Make push.sh re-entrant
    Instead of having tunnel check on the app status and bail, change push.sh so it
    simply exits if the app is already running and have tunnel call push.sh unconditionally.

--- push.sh
+++ push.sh
@@ -5,12 +5,17 @@ set -e # exit on error
 cd `dirname $0`
 source .default.sh
 
-appname="cf-ssh-chisel-$USER"
+# See if we're already running...
+echo "Checking if $CHISEL_APP_NAME is running"
+if cf app $CHISEL_APP_NAME | grep -q running; then
+  echo "$CHISEL_APP_NAME is running; skipping push."
+  exit
+fi
 
 # Generate a key to identify the server (if one doesn't already exist)
 # TODO: put all generated files (this and id_rsa.pub) into a single directory
 if [ ! -r ssh_host_rsa_key ]; then
-  ssh-keygen -t rsa -f ssh_host_rsa_key -N '' -C "chisel-ssh identity for $appname"
+  ssh-keygen -t rsa -f ssh_host_rsa_key -N '' -C "chisel-ssh identity for $CHISEL_APP_NAME"
   echo '[localhost]:$CHISEL_LOCAL_PORT' `cat ssh_host_rsa_key.pub` >> ~/.ssh/known_hosts
 fi
 
@@ -34,7 +39,7 @@ Host chisel
 _EOF_
 fi
 
-cf push -t 180 $appname & # -t: maximum number of seconds to wait for app to start
+cf push -t 180 "$CHISEL_APP_NAME" & # -t: maximum number of seconds to wait for app to start
 
 export GOPATH=$(echo ${PWD%src/github.com/jpillora/chisel})
 go build
